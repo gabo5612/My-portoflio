40b5e7f7d4251d6230db6478f47430456dc68a32
Gabo <gabo5612@gmail.com>
2023-02-03 19:18:45 -0400

    listo

---
+++ Calculator/main.js
@@ -0,0 +1,62 @@
+/* hacer que cuando se haga click en los simbolos se ponga background-color: #ffffff;
+    color: #f6a20b; 
+    hacer que cuando se ponga un numero se cambie el ac a C*/
+
+const  deleter=document.getElementsByClassName('delete');
+const  valor=document.getElementsByClassName('valor');
+const  percentage=document.getElementsByClassName('percentage');
+const  divider=document.getElementsByClassName('divider');
+const  seven=document.getElementsByClassName('seven');
+const  eight=document.getElementsByClassName('eight');
+const  nine=document.getElementsByClassName('nine');
+const  multiplier=document.getElementsByClassName('multiplier');
+const  four=document.getElementsByClassName('four');
+const  five=document.getElementsByClassName('five');
+const  six=document.getElementsByClassName('six');
+const  negative=document.getElementsByClassName('negative');
+const  one=document.getElementsByClassName('one');
+const  two=document.getElementsByClassName('two');
+const  three=document.getElementsByClassName('three');
+const  plus=document.getElementsByClassName('plus');
+const  zero=document.getElementsByClassName('zero');
+const  comma=document.getElementsByClassName('comma');
+const  equal=document.getElementsByClassName('equal');
+const screen=document.getElementsByClassName('screen');
+var resultado=0;
+
+
+deleter.addEventListener('click',catcher);
+valor.addEventListener('click',catcher);
+percentage.addEventListener('click',catcher);
+divider.addEventListener('click',catcher);
+seven.addEventListener('click',catcher);
+eight.addEventListener('click',catcher);
+nine.addEventListener('click',);
+multiplier.addEventListener('click',catcher);
+four.addEventListener('click',catcher);
+five.addEventListener('click',catcher);
+six.addEventListener('click',catcher);
+negative.addEventListener('click',catcher);
+one.addEventListener('click',catcher);
+two.addEventListener('click',catcher);
+three.addEventListener('click',catcher);
+plus.addEventListener('click',catcher);
+zero.addEventListener('click',catcher);
+comma.addEventListener('click',catcher);
+equal.addEventListener('click',catcher);
+
+function catcher()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
