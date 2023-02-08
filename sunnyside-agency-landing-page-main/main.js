5d42b148fee18625d79a85e60f784d5d7b67999d
Gabo <gabo5612@gmail.com>
2023-02-08 17:42:06 -0400

    Falta la version desktop

---
+++ sunnyside-agency-landing-page-main/main.js
@@ -0,0 +1,8 @@
+const menuMobile=document.querySelector('.mobile');
+const hmMenu=document.querySelector('#hamburger');
+
+hmMenu.addEventListener('click',function(){
+    menuMobile.classList.toggle('inactive')
+    
+})
+
